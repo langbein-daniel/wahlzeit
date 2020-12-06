e7a3c83b3bc5ecff35721b75e0cb82dbfb200d50
Daniel Langbein <daniel@systemli.org>
2020-12-06 22:52:14 +0100

    minor changes

--- src/main/java/org/wahlzeit/model/location/AbstractCoordinate.java
+++ src/main/java/org/wahlzeit/model/location/AbstractCoordinate.java
@@ -1,20 +1,19 @@
 package org.wahlzeit.model.location;
 
 import org.wahlzeit.services.DataObject;
-import org.wahlzeit.utils.DoubleUtil;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Note: A static Coordinate object representing the center can be found in subclass CartesianCoordinate.
+ */
 public abstract class AbstractCoordinate extends DataObject implements Coordinate {
-    /*
-     * A static CENTER Coordinate can be found in subclass CartesianCoordinate
-     */
 
     /**
-     * digits to the right of the decimal point that are taken into account
-     * when comparing and for hash code generation of double values
+     * Digits to the right of the decimal point that are taken into account
+     * when comparing and for hash code generation of double values.
      */
     public static final int SCALE = 12;
 
@@ -59,25 +58,42 @@ public abstract class AbstractCoordinate extends DataObject implements Coordinat
     /**
      * @methodtype comparison
      * @methodproperties composed
+     *
+     * A Coordinate shall be comparable/identifiable by the point in space it represents no matter of the internal
+     * representation (spherical, cartesian, cylindrical, etc.  coordinate system).
+     * Therefore we do first convert all of them to CartesianCoordinates and then compare two
+     * CartesianCoordinates for equality.
      */
     @Override
     public boolean isEqual(Coordinate other) {
         if (other == null) return false;
-        return this.toString().equals(other.toString());
+
+        String thisCartesianString = asCartesianCoordinate().toString();
+        String otherCartesianString = other.asCartesianCoordinate().toString();
+        return thisCartesianString.equals(otherCartesianString);
     }
 
     /**
      * @methodtype conversion
      * @methodproperties composed
+     *
+     * A Coordinate shall be comparable/identifiable by the point in space it represents no matter of the internal
+     * representation (spherical, cartesian, cylindrical, etc.  coordinate system).
+     * Therefore we do first convert all of them to CartesianCoordinates and then generate a hash.
      */
     @Override
     public int hashCode() {
-        /*
-         * A Coordinate shall be comparable/identifiable by the point in space it represents no matter of the internal representation
-         * (spherical, cartesian, cylindrical, etc.  coordinate system).
-         * Therefore we do first convert all of them to CartesianCoordinates.
-         */
         return asCartesianCoordinate().toString().hashCode();
+    }
+
+    /**
+     * Default implementation (for subclasses other than CartesianCoordinate)
+     * so that one can print them with more details than with the default Object.toString() method.
+     * Though, one may overwrite this method in a subclasses.
+     */
+    @Override
+    public String toString() {
+        return this.asCartesianCoordinate().toString();
     }
 
     //=== Persistence Methods ===
